82839fa61122c4069b675d2db7cffcc2aa124818
annshev1avis <annshev1220@gmail.com>
2024-04-08 00:36:57 +0300

    прописаны первые маршруты API (файл urls.py) и их обработчики

---
+++ ideika_site/api/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+from django.views.decorators.csrf import csrf_exempt
+
+
+"""
+TODO: убрать csrf_exempt()
+временно все функции обернуты в csrf_exempt(func), чтобы был отключен встроеннный 
+механизм защиты от csrf-атак. Он заключается в том, при каждой новой сессии пользователю
+выдается токен, без которого не проходят POST, PUT, DELETE запросы.
+Чтобы облегчить тестирование, защита выключена
+
+https://stackoverflow.com/questions/50732815/how-to-use-csrf-token-in-django-restful-api-and-react
+"""
+urlpatterns = [
+    path('', views.index),
+    path('cards/', csrf_exempt(views.CardsView.as_view())),
+    path('cards/<int:card_id>/', csrf_exempt(views.CardsIdView.as_view())),
+    #path('users/<int:user_id>/cards/<int:card_id>/', views.add_card_to_collection)
+]
--- ideika_site/api/views.py
+++ ideika_site/api/views.py
@@ -1,3 +1,31 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+
+def index(request): #HttpRequest
+    return HttpResponse("this is ideika API!")
+
+
+"""пока что обрабочики не выполняют задуманный функционал, но работают"""
+class CardsView(View):
+    def get(self, request):
+        return HttpResponse(f"all cards")
+    def post(self, request):
+        return HttpResponse("create new card")
+
+
+class CardsIdView(View):
+    def get(self, request, card_id):
+        return HttpResponse(f"get card {card_id}")
+
+    def put(self, request, card_id):
+        return HttpResponse(f"update card {card_id}")
+
+    def delete(self, request, card_id):
+        return HttpResponse(f"delete card {card_id}")
+
+
+def add_card_to_collection(request, user_id, card_id):
+    return HttpResponse(f"add card {card_id} to user's {user_id} collection")
