d520f40fa22735fc7a5b3d62057775adb007dca9
annshev1avis <annshev1220@gmail.com>
2024-04-12 18:21:07 +0300

    Добавлены view-классы CardsView, CardsIdView, CardUser, CardTag. А потом мне захотелось писать на DjangoRESTframework

--- ideika_site/api/urls.py
+++ ideika_site/api/urls.py
@@ -12,9 +12,11 @@ TODO: убрать csrf_exempt()
 
 https://stackoverflow.com/questions/50732815/how-to-use-csrf-token-in-django-restful-api-and-react
 """
+
 urlpatterns = [
     path('', views.index),
     path('cards/', csrf_exempt(views.CardsView.as_view())),
     path('cards/<int:card_id>/', csrf_exempt(views.CardsIdView.as_view())),
-    #path('users/<int:user_id>/cards/<int:card_id>/', views.add_card_to_collection)
+    path('users/<int:user_id>/cards/', csrf_exempt(views.CardUser.as_view())),
+    path('users/<int:user_id>/cards/<int:card_id>/', csrf_exempt(views.CardUser.as_view()))
 ]
--- ideika_site/api/views.py
+++ ideika_site/api/views.py
@@ -1,21 +1,49 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views import View
-
+from api.models import Card, User, CardInUserCollection
+import json
 
 
 def index(request): #HttpRequest
     return HttpResponse("this is ideika API!")
 
 
-"""пока что обрабочики не выполняют задуманный функционал, но работают"""
+"""добавить сортировки: сначала новые, сначала старые, по алфавиту"""
+# /cards
 class CardsView(View):
     def get(self, request):
-        return HttpResponse(f"all cards")
+
+        if "collection_id" in request.GET:
+            cards = Card.objects.filter(category_id=request.GET["category_id"])
+        else:
+            cards = Card.objects.all()
+
+        cards_list = []
+        for card in cards:
+            cards_list.append({"id": card.id,
+                             "name": card.name,
+                             "desc": card.description,
+                             "own": False})
+
+        # добавляет информацию о том, если ли карточка в коллекции пользователя
+        if "user_id" in request.GET:
+            collection_ids = [c.id for c in CardInUserCollection.objects.filter(user_id=request.GET["user_id"])]
+            for i in range(len(cards_list)):
+                cards_list[i]["own"] =  cards_list[i]["id"] in collection_ids
+
+        return JsonResponse(cards_list, safe=False)
+
     def post(self, request):
+        body = json.loads(request.body.decode('utf-8'))
+
+        new_card = Card(name=body["name"], description=body["description"], creator_id=body["creator_id"],
+                        category_id=body["category_id"])
+        new_card.save()
+
         return HttpResponse("create new card")
 
-
+# cards/4/
 class CardsIdView(View):
     def get(self, request, card_id):
         return HttpResponse(f"get card {card_id}")
@@ -26,6 +54,28 @@ class CardsIdView(View):
     def delete(self, request, card_id):
         return HttpResponse(f"delete card {card_id}")
 
+# users/1/cards/4
+class CardUser(View):
+    def get(self, request, user_id):
+        cards = []
+        cards_in_collection_ids = [x.id for x in CardInUserCollection.objects.filter(user_id=user_id)]
+        watched_ids = [x.id for x in CardInUserCollection.objects.filter(is_watched=1)]
+        for card in Card.objects.filter(id__in=cards_in_collection_ids):
+            cards.append({
+                "id": card.id,
+                "name": card.name,
+                "description": card.description,
+                "watched": card.id in watched_ids
+            })
 
-def add_card_to_collection(request, user_id, card_id):
-    return HttpResponse(f"add card {card_id} to user's {user_id} collection")
+        return HttpResponse(cards)
+
+    def post(self, request, user_id, card_id):
+        return HttpResponse(f"new card {card_id} for user {user_id}")
+
+
+# cards/1/tags/2
+class CardTag(View):
+    def get(self):
+        pass
+
